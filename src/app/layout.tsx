562d06bba8199d11d95b036f2484b5bc5d36b45e
Ahad Aziz Jaffer <k213241@nu.edu.pk>
2026-04-06 01:42:19 +0500

    updated favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
   description:
     "Transform massive datasets into interactive dashboards. Entirely in your browser.",
   icons: {
-    icon: "/Metrx icon.png",
+    icon: "/favicon.ico",
   },
 };
 
